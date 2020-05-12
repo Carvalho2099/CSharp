03770740f4428cbc79770783ba48b49e244f706e
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-05-12 10:06:51 -0400

    Comentários de Código

---
+++ Fundamentos/CursoCSharp/CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Comentarios
+    {
+        public static void Executar()
+        {
+            //Cuidado com os comentários desnecessários
+            Console.WriteLine("código claro é sempre melhor");
+            /*
+             Comentarios de multiplas linhas....
+             */
+
+            Console.WriteLine("o C# tem o XML Comments");
+        }
+    }
+}
